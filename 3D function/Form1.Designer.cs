bfab2d952d44a43aa95f618b24ea1a122716f1b8
Moskalenko Sergey <oknelaksoms@gmail.com>
2020-06-28 13:55:25 +0300

    version 0.2

--- 3D function/Form1.Designer.cs
+++ 3D function/Form1.Designer.cs
@@ -39,11 +39,14 @@
             this.label6 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.trackBar6 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar6)).BeginInit();
             this.SuspendLayout();
             // 
             // glControl1
@@ -58,6 +61,7 @@
             // trackBar1
             // 
             this.trackBar1.BackColor = System.Drawing.Color.Red;
+            this.trackBar1.LargeChange = 1;
             this.trackBar1.Location = new System.Drawing.Point(12, 39);
             this.trackBar1.Maximum = 5;
             this.trackBar1.Minimum = -5;
@@ -68,6 +72,7 @@
             // trackBar2
             // 
             this.trackBar2.BackColor = System.Drawing.Color.Blue;
+            this.trackBar2.LargeChange = 1;
             this.trackBar2.Location = new System.Drawing.Point(12, 90);
             this.trackBar2.Maximum = 5;
             this.trackBar2.Minimum = -5;
@@ -78,6 +83,7 @@
             // trackBar3
             // 
             this.trackBar3.BackColor = System.Drawing.Color.Green;
+            this.trackBar3.LargeChange = 1;
             this.trackBar3.Location = new System.Drawing.Point(12, 141);
             this.trackBar3.Maximum = 5;
             this.trackBar3.Minimum = -5;
@@ -88,13 +94,14 @@
             // trackBar5
             // 
             this.trackBar5.BackColor = System.Drawing.Color.White;
+            this.trackBar5.LargeChange = 1;
             this.trackBar5.Location = new System.Drawing.Point(9, 269);
-            this.trackBar5.Maximum = 15;
-            this.trackBar5.Minimum = -15;
+            this.trackBar5.Maximum = 10000;
             this.trackBar5.Name = "trackBar5";
             this.trackBar5.Size = new System.Drawing.Size(233, 45);
             this.trackBar5.TabIndex = 23;
             this.trackBar5.Value = 5;
+            this.trackBar5.Scroll += new System.EventHandler(this.TrackBar5_Scroll);
             // 
             // label7
             // 
@@ -108,13 +115,15 @@
             // trackBar4
             // 
             this.trackBar4.BackColor = System.Drawing.Color.White;
+            this.trackBar4.LargeChange = 1;
             this.trackBar4.Location = new System.Drawing.Point(9, 205);
-            this.trackBar4.Maximum = 20;
-            this.trackBar4.Minimum = -20;
+            this.trackBar4.Maximum = 50;
+            this.trackBar4.Minimum = 1;
             this.trackBar4.Name = "trackBar4";
             this.trackBar4.Size = new System.Drawing.Size(233, 45);
             this.trackBar4.TabIndex = 21;
             this.trackBar4.Value = 5;
+            this.trackBar4.Scroll += new System.EventHandler(this.TrackBar4_Scroll);
             // 
             // label6
             // 
@@ -136,12 +145,40 @@
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(230, 21);
             this.comboBox1.TabIndex = 24;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // trackBar6
+            // 
+            this.trackBar6.BackColor = System.Drawing.Color.White;
+            this.trackBar6.Enabled = false;
+            this.trackBar6.LargeChange = 1;
+            this.trackBar6.Location = new System.Drawing.Point(9, 333);
+            this.trackBar6.Maximum = 20;
+            this.trackBar6.Minimum = 1;
+            this.trackBar6.Name = "trackBar6";
+            this.trackBar6.Size = new System.Drawing.Size(233, 45);
+            this.trackBar6.TabIndex = 25;
+            this.trackBar6.Value = 3;
+            this.trackBar6.Visible = false;
+            this.trackBar6.Scroll += new System.EventHandler(this.TrackBar6_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 317);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Количество спиралей";
+            this.label1.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(415, 367);
+            this.ClientSize = new System.Drawing.Size(589, 568);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar6);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.trackBar5);
             this.Controls.Add(this.label7);
@@ -160,6 +197,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar6)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -177,6 +215,8 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TrackBar trackBar6;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- 3D function/Form1.cs
+++ 3D function/Form1.cs
@@ -23,127 +23,125 @@ namespace _3D_function
         Vector3d[] a0;
         Vector3d[] a1;
         Vector3d[] a2;
+        int smth=14400;
         private void Form1_Load(object sender, EventArgs e)
         {
             comboBox1.Items.Add("DNA");
-            comboBox1.Items.Add("");
-            a0 = new Vector3d[7200];
-            a1 = new Vector3d[7200];
-            a2 = new Vector3d[7200];
+            comboBox1.Items.Add("Function1");
+            comboBox1.Items.Add("Function2");
+            comboBox1.Items.Add("Function3");
+            comboBox1.Items.Add("Function4");
+            comboBox1.Items.Add("Function5");
+            comboBox1.Items.Add("Function6");
+            a0 = new Vector3d[smth*3];
             timer1.Interval = 1;
             timer1.Enabled = true;
             GL.ClearColor(Color.FromArgb(0, 0, 0));
             GL.Clear(ClearBufferMask.ColorBufferBit);
+            xyz = new Vector3d[6];
+            coord_os();
         }
         Vector3d[] xyz;//координатные оси
         private void timer1_Tick(object sender, EventArgs e)
         {
             if (comboBox1.Text=="DNA")
             {
-                xyz = new Vector3d[6];
-                GL.ClearColor(Color.Black);
-                GL.Clear(ClearBufferMask.ColorBufferBit);
-                GL.Begin(PrimitiveType.Lines);
-                coord_os();
-                GL.Color3(Color.Red);//ox
-                GL.Vertex3(xyz[0]);
-                GL.Vertex3(xyz[1]);
-                GL.Color3(Color.Blue);//oy
-                GL.Vertex3(xyz[2]);
-                GL.Vertex3(xyz[3]);
-                GL.Color3(Color.Green);//oz
-                GL.Vertex3(xyz[4]);
-                GL.Vertex3(xyz[5]);
-                GL.End();
-                double r = 0.4, A = 5;
-                r = trackBar4.Value * 0.05;//Амплитуда
-                A = trackBar5.Value;//Частота
-                GL.Begin(PrimitiveType.Points);
-                for (int i = -3600; i < 3600; i++)
-                {
-                    a0[i + 3600].X = i * 0.001;
-                    a0[i + 3600].Y = r * Math.Cos(a0[i + 3600].X * A);
-                    a0[i + 3600].Z = r * Math.Sin(a0[i + 3600].X * A);
-                }
-                //r = 0.4;
-                //A=5;
-                for (int i = -3600; i < 3600; i++)
-                {
-                    a1[i + 3600].X = i * 0.001;
-                    a1[i + 3600].Y = r * Math.Cos(a1[i + 3600].X * A + 90);
-                    a1[i + 3600].Z = r * Math.Sin(a1[i + 3600].X * A + 90);//... + 90) - здвиг на фазу
-                }
-                //r = 0.4;
-                //A = 5;
-                for (int i = -3600; i < 3600; i++)
-                {
-                    a2[i + 3600].X = i * 0.001;
-                    a2[i + 3600].Y = r * Math.Cos(a2[i + 3600].X * A + 180);
-                    a2[i + 3600].Z = r * Math.Sin(a2[i + 3600].X * A + 180);
-                }
-                GL.Color3(Color.White);
-                for (int i = 0; i < 7200; i++)
-                {
-                    GL.Color3(Color.White);
-                    GL.Vertex3(a0[i]);
-                    GL.Color3(Color.White);
-                    GL.Vertex3(a1[i]);
-                    GL.Color3(Color.White);
-                    GL.Vertex3(a2[i]);
-                }
-                GL.End();
-                GL.Begin(PrimitiveType.Lines);
-                GL.Color3(Color.DarkOrange);
-                int w = 0;
-                for (int i = 0; i < 7200; i += 100)
-                {
-                    w++;
-                    if (w == 10)
-                    {
-                        GL.Color3(Color.Yellow);
-                        w = 0;
-                    }
-                    else
-                        GL.Color3(Color.DodgerBlue);
-                    GL.Vertex3(a0[i]);
-                    GL.Vertex3(a1[i]);
-                    GL.Vertex3(a0[i]);
-                    GL.Vertex3(a2[i]);
-                    GL.Vertex3(a1[i]);
-                    GL.Vertex3(a2[i]);
-                    GL.Vertex3(a0[i]);
-                }
-                GL.End();
-                GL.Begin(PrimitiveType.Patches);
-                GL.Color3(Color.White);
-                if (!(trackBar1.Value == 0 && trackBar2.Value == 0 && trackBar3.Value == 0))
-                {
-                    GL.Rotate(1, trackBar1.Value, trackBar2.Value, trackBar3.Value);
-                }
-                //GL.Rotate(1, R.NextDouble(), R.NextDouble(), R.NextDouble());
-                GL.End();
-                GL.Flush();
-                glControl1.SwapBuffers();
-
-                //speed--;
-                //GL.ClearColor(Color.FromArgb(0, 0, 0));
-                //GL.Clear(ClearBufferMask.ColorBufferBit);
-                //GL.Begin(PrimitiveType.Lines);
-                ////fractal4
-                //GL.End();
-                //GL.Begin(PrimitiveType.Patches);
-                //GL.Rotate(speed/100,(y_down - y_up), (x_down - x_up),0 );
-                //GL.End();
-                //glControl1.SwapBuffers();
-                //if (speed <= 0)
-                //{
-                //    timer1.Enabled = false;
-                //}
+                draw_DNA();
             }
-            if(comboBox1.Text=="")
+            //if(comboBox1.Text=="Spiral")
+            //{//трехмерная спираль
+
+            //}
+            else if(comboBox1.SelectedIndex==6)
             {
-
+                draw_Sphere();
             }
+            else
+            {
+                draw_F1();
+            }
+        }
+
+        void draw_DNA()
+        {
+            GL.ClearColor(Color.Black);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(Color.Red);//ox
+            GL.Vertex3(xyz[0]);
+            GL.Vertex3(xyz[1]);
+            GL.Color3(Color.Blue);//oy
+            GL.Vertex3(xyz[2]);
+            GL.Vertex3(xyz[3]);
+            GL.Color3(Color.Green);//oz
+            GL.Vertex3(xyz[4]);
+            GL.Vertex3(xyz[5]);
+            GL.End();
+            double r = 0.4, A = 5;
+            A = trackBar4.Value * 0.01;//Амплитуда
+            r = trackBar5.Value;//Частота
+            GL.Begin(PrimitiveType.Points);
+            int koef = 0, num = trackBar6.Value;
+            for (int j = 0; j < num; j++)
+            {
+                for (int i = 0; i < smth; i++)
+                {
+                    a0[i].X = (i-3600) * 0.001;
+                    a0[i].Y = A * Math.Cos(a0[i].X * r + (koef * Math.PI) / 180);
+                    a0[i].Z = A * Math.Sin(a0[i].X * r + (koef * Math.PI) / 180);
+                    GL.Color3(Color.White);
+                    GL.Vertex3(a0[i]);
+                }
+                koef += 360 / num;
+            }
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(Color.DarkOrange);
+            int w = 0;
+            /*
+            for (int i = 0; i < 7200; i += 100)
+            {
+                w++;
+                if (w == 10)
+                {
+                    GL.Color3(Color.Yellow);
+                    w = 0;
+                }
+                else
+                    GL.Color3(Color.DodgerBlue);
+                GL.Vertex3(a0[i]);
+                GL.Vertex3(a1[i]);
+                GL.Vertex3(a0[i]);
+                GL.Vertex3(a2[i]);
+                GL.Vertex3(a1[i]);
+                GL.Vertex3(a2[i]);
+                GL.Vertex3(a0[i]);
+            }*/
+            GL.End();
+            GL.Begin(PrimitiveType.Patches);
+            GL.Color3(Color.White);
+            if (!(trackBar1.Value == 0 && trackBar2.Value == 0 && trackBar3.Value == 0))
+            {
+                GL.Rotate(1, trackBar1.Value, trackBar2.Value, trackBar3.Value);
+            }
+            //GL.Rotate(1, R.NextDouble(), R.NextDouble(), R.NextDouble());
+            GL.End();
+            GL.Flush();
+            glControl1.SwapBuffers();
+
+            //speed--;
+            //GL.ClearColor(Color.FromArgb(0, 0, 0));
+            //GL.Clear(ClearBufferMask.ColorBufferBit);
+            //GL.Begin(PrimitiveType.Lines);
+            ////fractal4
+            //GL.End();
+            //GL.Begin(PrimitiveType.Patches);
+            //GL.Rotate(speed/100,(y_down - y_up), (x_down - x_up),0 );
+            //GL.End();
+            //glControl1.SwapBuffers();
+            //if (speed <= 0)
+            //{
+            //    timer1.Enabled = false;
+            //}
         }
 
         void coord_os()
@@ -166,6 +164,204 @@ namespace _3D_function
             xyz[5].X = 0;
             xyz[5].Y = 0;
             xyz[5].Z = -1;
+        }
+
+        void draw_F1()
+        {
+            GL.ClearColor(Color.Black);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            //GL.Begin(PrimitiveType.Lines);
+            //GL.Color3(Color.Red);//ox
+            //GL.Vertex3(xyz[0]);
+            //GL.Vertex3(xyz[1]);
+            //GL.Color3(Color.Blue);//oy
+            //GL.Vertex3(xyz[2]);
+            //GL.Vertex3(xyz[3]);
+            //GL.Color3(Color.Green);//oz
+            //GL.Vertex3(xyz[4]);
+            //GL.Vertex3(xyz[5]);
+            //GL.End();
+            double r = 0.4, A = 5;
+            A = trackBar4.Value * 0.00001;//Амплитуда
+            r = trackBar5.Value * 0.001;//Частота
+            //GL.Begin(PrimitiveType.Lines);
+            GL.Begin(PrimitiveType.Points);
+            //int koef = 0, num = trackBar6.Value;
+            //for (int j = 0; j < num; j++)
+            //{
+            a0[0].X = 0;
+            a0[0].Y = 0;
+            a0[0].Z = 0;
+            if (comboBox1.SelectedIndex == 1)
+                for (int i = 1; i < smth; i++)
+                {
+                    GL.Vertex3(a0[i - 1]);
+                    //v1:
+                    a0[i].X = A * i * Math.Sin(r * i * Math.PI / 180);
+                    a0[i].Y = A * i * Math.Cos(Math.Sin(r * i * Math.PI / 180) * i * Math.PI / 180);
+                    a0[i].Z = A * i * Math.Sin(r * Math.Sqrt(i) * Math.PI / 180);
+                    GL.Color3(Color.White);
+                    //GL.Vertex3(a0[i]);
+                }
+            else if (comboBox1.SelectedIndex == 2)
+                for (int i = 1; i < smth; i++)
+                {
+                    GL.Vertex3(a0[i - 1]);
+                    //v2:
+                    a0[i].X = A * i * Math.Cos(r * i * Math.PI / 180);
+                    a0[i].Y = A * i * Math.Atan(r * i * Math.PI / 180 + (Math.PI / 2));
+                    a0[i].Z = A * i * Math.Sin(r * i * Math.PI / 180 + (i * Math.PI / 360));
+                    GL.Color3(Color.White);
+                    //GL.Vertex3(a0[i]);
+                }
+            else if (comboBox1.SelectedIndex == 3)
+                for (int i = 1; i < smth; i++)
+                {
+                    GL.Vertex3(a0[i - 1]);
+                    //v3:
+                    a0[i].X = A * i * Math.Sin(r * i * Math.PI / 180);
+                    a0[i].Y = A * i * Math.Atan(r * i * Math.PI / 180) * Math.Sin(r * i * Math.PI / 180 + (i * Math.PI / 360));
+                    a0[i].Z = A * i * Math.Cos(r * i * Math.PI / 180);
+                    GL.Color3(Color.White);
+                    //GL.Vertex3(a0[i]);
+                }
+            else if (comboBox1.SelectedIndex == 4)
+                for (int i = 1; i < smth; i++)
+                {
+                    GL.Vertex3(a0[i - 1]);
+                    //v4:
+                    a0[i].X = A * i * Math.Cos(i * r * Math.PI / 180) / Math.Log(i, smth);
+                    a0[i].Y = A * i * Math.Sin(i * r * Math.PI / 180);
+                    a0[i].Z = A * i * Math.Sin(i * Math.PI / 180);
+                    GL.Color3(Color.White);
+                    //GL.Vertex3(a0[i]);
+                }
+            else if (comboBox1.SelectedIndex == 5)
+                for (int i = 1; i < smth; i++)
+                {
+                    GL.Vertex3(a0[i - 1]);
+                    //v5:
+                    a0[i].X = A * i *Math.Sin(r * i * Math.PI / 180);
+                    a0[i].Y = A * i * Math.Cos(Math.Log(i, 2)*r * Math.PI / 180) ;
+                    a0[i].Z = A * i *Math.Log(Math.Cos(r * i * Math.PI / 180), Math.Sqrt(i));
+                    GL.Color3(Color.White);
+                    //GL.Vertex3(a0[i]);
+                }
+            
+            //koef += 360 / num;
+            //}
+            GL.End();
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(Color.DarkOrange);
+            int w = 0;
+            GL.End();
+            GL.Begin(PrimitiveType.Patches);
+            GL.Color3(Color.White);
+            if (trackBar1.Value != 0 || trackBar2.Value != 0 || trackBar3.Value != 0)
+            {
+                GL.Rotate(0.4, trackBar1.Value, trackBar2.Value, trackBar3.Value);
+            }
+            //GL.Rotate(1, R.NextDouble(), R.NextDouble(), R.NextDouble());
+            GL.End();
+            GL.Flush();
+            glControl1.SwapBuffers();
+
+            //speed--;
+            //GL.ClearColor(Color.FromArgb(0, 0, 0));
+            //GL.Clear(ClearBufferMask.ColorBufferBit);
+            //GL.Begin(PrimitiveType.Lines);
+            ////fractal4
+            //GL.End();
+            //GL.Begin(PrimitiveType.Patches);
+            //GL.Rotate(speed/100,(y_down - y_up), (x_down - x_up),0 );
+            //GL.End();
+            //glControl1.SwapBuffers();
+            //if (speed <= 0)
+            //{
+            //    timer1.Enabled = false;
+            //}
+        }
+
+        void draw_Sphere()
+        {
+            GL.ClearColor(Color.Black);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            double r = 0.4, A = 5;
+            A = trackBar4.Value * 0.00001;//Амплитуда
+            r = trackBar5.Value * 0.001;//Частота
+            //GL.Begin(PrimitiveType.Lines);
+            GL.Begin(PrimitiveType.Points);
+            //int koef = 0, num = trackBar6.Value;
+            //for (int j = 0; j < num; j++)
+            //{
+            a0[0].X = 0;
+            a0[0].Y = 0;
+            a0[0].Z = 0;
+            for (int i = 0; i <= smth / 2; i++)
+            {
+                for (int j = 0; j < smth; j++)
+                {
+                    GL.Vertex3(a0[i + j]);
+                    //v5:
+                    a0[i + j].X = A * 2000 * Math.Sin(i * Math.PI / 180) * Math.Cos(j * Math.PI / 180);
+                    a0[i + j].Y = A * 2000 * Math.Sin(i * Math.PI / 180) * Math.Sin(j * Math.PI / 180);
+                    a0[i + j].Z = A * 2000 * Math.Cos(i * Math.PI / 180);
+                    GL.Color3(Color.White);
+                    //GL.Vertex3(a0[i]);
+                }
+            }
+            GL.End();
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(Color.DarkOrange);
+            int w = 0;
+            GL.End();
+            GL.Begin(PrimitiveType.Patches);
+            GL.Color3(Color.White);
+            if (trackBar1.Value != 0 || trackBar2.Value != 0 || trackBar3.Value != 0)
+            {
+                GL.Rotate(0.4, trackBar1.Value, trackBar2.Value, trackBar3.Value);
+            }
+            GL.End();
+            GL.Flush();
+            glControl1.SwapBuffers();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            smth = 28800;
+            a0 = new Vector3d[smth];
+            if (comboBox1.SelectedIndex == 0)
+            {
+
+                label1.Visible = true;
+                trackBar6.Visible = true;
+                trackBar6.Enabled = true;
+            }
+            else
+            {
+                label1.Visible = false;
+                trackBar6.Visible = false;
+            }
+            if (comboBox1.SelectedIndex == 6)
+            {
+                smth = 360;
+                a0 = new Vector3d[smth * 2];
+            }
+        }
+
+        private void TrackBar6_Scroll(object sender, EventArgs e)
+        {
+            label1.Text=trackBar6.Value.ToString();
+        }
+
+        private void TrackBar5_Scroll(object sender, EventArgs e)
+        {
+            label6.Text = trackBar5.Value.ToString();
+        }
+
+        private void TrackBar4_Scroll(object sender, EventArgs e)
+        {
+            label7.Text = trackBar4.Value.ToString();
         }
     }
 }
